45c93f032ef4150cb3b61cb6e66acaae54a3fd4d
RRoundTable <ryu071511@gmail.com>
2020-04-30 23:31:36 -0700

    add pca feautere

--- dataframe_manager.py
+++ dataframe_manager.py
@@ -1,4 +1,54 @@
 import pandas as pd
+from sklearn.decomposition import PCA
+
+TARGET = 'sales'
+SEED = 1
+
+def make_pca(
+    df:pd.DataFrame,
+    pca_col: str,
+    n_days: int,
+    keep_dim: int = 3
+):
+    print('PCA:', pca_col, n_days)
+    pca_df = df[[pca_col, 'd', TARGET]]
+
+    if pca_col != 'id':
+        merge_base = pca_df[[pca_col,'d']]
+        pca_df = pca_df.groupby([pca_col,'d'])[TARGET].agg(['sum']).reset_index()
+        pca_df[TARGET] = pca_df['sum']
+        del pca_df['sum']
+
+    # min-max scaling
+    pca_df[TARGET] = pca_df[TARGET]/pca_df[TARGET].max()
+    LAG_DAYS = [col for col in range(1,n_days+1)]
+    format_s = '{}_pca_'+pca_col+str(n_days)+'_{}'
+    
+    pca_df = pca_df.assign(**{
+            format_s.format(col, l): pca_df.groupby([pca_col])[col].transform(lambda x: x.shift(l))
+            for l in LAG_DAYS
+            for col in [TARGET]
+            })
+    
+    res_cols = list(pca_df)[3:]
+    pca_df[pca_columns] = pca_df[pca_columns].fillna(0)
+    
+    # define PCA
+    pca = PCA(random_state=SEED)
+    pca.fit(pca_df[res_cols])
+    pca_df[res_cols] = pca.transform(pca_df[res_cols])
+    print("Explained variance ratio: ", pca.explained_variance_ratio)
+    
+    keep_cols = res_cols[:keep_dim]
+    print("Columns to keep: ", keep_cols)
+
+    return pca_df[keep_cols]
+
+
+
+def make_normal_lag(lag_day):
+    NotImplemented
+    
 
 
 def date_features(df: pd.DataFrame):
